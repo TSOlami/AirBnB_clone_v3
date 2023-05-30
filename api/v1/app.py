f023c20760e39c2c66b285621aa82b6a7bbfa7aa
TSOlami <tijani.saheed@yahoo.com>
2023-05-30 17:37:36 +0100

    AirBnB_clone_v3

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,39 +1,3 @@
-<<<<<<< HEAD
-#!/usr/bin/python3
-"""
-app
-"""
-from flask import Flask, jsonify, make_response
-from models import storage
-from api.v1.views import app_views
-from os import getenv
-from flask_cors import CORS
-
-app = Flask(__name__)
-app.register_blueprint(app_views)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
-
-
-@app.teardown_appcontext
-def close():
-    """calls the storage close method"""
-    storage.close()
-
-
-@app.errorhandler(404)
-def not_found():
-    """creates a 404 page"""
-    return make_response(jsonify({"error": "NOt Found"}), 404)
-
-
-if __name__ == "__main__":
-
-    host = getenv('HBNB_API_HOST', default='0.0.0.0')
-    port = getenv('HBNB_API_PORT', default='5000')
-
-    app.run(host, int(port), threaded=True)
-=======
 #!/usr/bin/python3
 """
 """
@@ -67,4 +31,3 @@ if __name__ == "__main__":
     port = getenv('HBNB_API_PORT', default='5000')
 
     app.run(host, int(port), threaded=True)
->>>>>>> 7f43dbc (AirBnB_clone_v3)
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,34 +1,7 @@
-<<<<<<< HEAD
 #!/usr/bin/python3
 """
+
 """
-from api.v1.views.places_amenities import *
-from api.v1.views.places_reviews import *
-from api.v1.views.places import *
-from api.v1.views.users import *
-from api.v1.views.amenities import *
-from api.v1.views.cities import *
-from api.v1.views.states import *
-from flask import Blueprint
 from api.v1.views.index import *
-
-
-app_views = Blueprint(app_views, __name__, url_prefix='/api/v1')
-=======
-#!/usr/bin/python3
-"""
-Init file
-"""
-from api.v1.views.places_amenities import *
-from api.v1.views.places_reviews import *
-from api.v1.views.places import *
-from api.v1.views.users import *
-from api.v1.views.amenities import *
-from api.v1.views.cities import *
-from api.v1.views.states import *
 from flask import Blueprint
-from api.v1.views.index import *
-
-
-app_views = Blueprint(app_views, __name__, url_prefix='/api/v1')
->>>>>>> 7f43dbc (AirBnB_clone_v3)
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
