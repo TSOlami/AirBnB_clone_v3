47e082bc93c215aacfba5f37b517cc2e6d3dfa12
TSOlami <tijani.saheed@yahoo.com>
2023-05-30 16:35:46 +0100

    AirBnB_clone_v3

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #!/usr/bin/python3
 """
 """
@@ -13,3 +14,21 @@ from api.v1.views.index import *
 
 
 app_views = Blueprint(app_views, __name__, url_prefix='/api/v1')
+=======
+#!/usr/bin/python3
+"""
+Init file
+"""
+from api.v1.views.places_amenities import *
+from api.v1.views.places_reviews import *
+from api.v1.views.places import *
+from api.v1.views.users import *
+from api.v1.views.amenities import *
+from api.v1.views.cities import *
+from api.v1.views.states import *
+from flask import Blueprint
+from api.v1.views.index import *
+
+
+app_views = Blueprint(app_views, __name__, url_prefix='/api/v1')
+>>>>>>> 7f43dbc (AirBnB_clone_v3)
